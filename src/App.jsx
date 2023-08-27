5197de79e4f6d915d6f64bd2c1799af2df100339
javohirtech <javohirtech@gmail.com>
2023-08-27 17:03:07 +0500

    add time for quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Question from "./components/Question";
 
 const sampleQuestions = [
@@ -40,6 +40,19 @@ function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
   const [answers, setAnswers] = useState([]);
+  const [timeLeft, setTimeLeft] = useState(5);
+
+  useEffect(() => {
+    if (timeLeft > 0 && currentQuestion < randomizedQuestions.length) {
+      const timerId = setTimeout(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+      return () => clearTimeout(timerId); // Clear the timer if the component is unmounted
+    } else if (timeLeft === 0) {
+      console.log(answers);
+      setCurrentQuestion(randomizedQuestions.length); // Finish the quiz
+    }
+  }, [timeLeft, currentQuestion, answers]);
 
   const handleAnswerOptionClick = (isCorrect, option) => {
     if (isCorrect) {
@@ -62,7 +75,7 @@ function App() {
   if (currentQuestion === randomizedQuestions.length) {
     return (
       <div className="app">
-        <h2>Quiz Summary</h2>
+        <h2>Quiz</h2>
         <div className="score-section">
           <h3>Your Score:</h3>
           <p>
@@ -84,6 +97,7 @@ function App() {
 
   return (
     <div className="app">
+      <div className="timer">Time left: {timeLeft} seconds</div>
       <Question
         key={currentQuestion}
         data={randomizedQuestions[currentQuestion]}
