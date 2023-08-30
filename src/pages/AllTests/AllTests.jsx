bbc019375ba4eee26529c3bab38b04cdbd2653c7
javohirtech <javohirtech@gmail.com>
2023-08-30 18:37:22 +0500

    AllTests is ready to work

--- src/pages/AllTests/AllTests.jsx
+++ src/pages/AllTests/AllTests.jsx
@@ -1,12 +1,31 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom"; // Import Link component
 import "./AllTests.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const AllTests = () => {
+  const [categories, setCategories] = useState([]);
+  const [categoryTestCount, setCategoryTestCount] = useState([]);
   const navigate = useNavigate();
   const isUserLogin = sessionStorage.getItem("enus");
   if (!isUserLogin) {
     navigate("/");
   }
+
+  const allTestCategories = () => {
+    const apiUrl = `http://192.168.0.150:8000/api/v1/admin/${isUserLogin}/category/list`;
+    axios.post(apiUrl).then((res) => {
+      setCategories(res.data.categories);
+      setCategoryTestCount(res.data.test_count);
+    });
+  };
+
+  // http://192.168.0.150:8000/api/v1/LXyx7aGciH7BT38U/test/listbycategory/matematika
+
+  useEffect(() => {
+    allTestCategories();
+  }, []);
+
   return (
     <div
       className="d-flex justify-content-center align-items-center"
@@ -14,38 +33,20 @@ const AllTests = () => {
     >
       <div className="all_tests_wrapper">
         <div className="test_cards">
-          <div className="test_card">
-            <small>Test soni: 40ta</small>
-            <h4 className="mb-3">Matematika</h4>
-            <small>Tuzuvchi: N.Abdullayeva</small>
-            <span>
-              Boshlash<i className="fas fa-arrow-right"></i>
-            </span>
-          </div>
-          <div className="test_card">
-            <small>Test soni: 40ta</small>
-            <h4 className="mb-3">Ona tili</h4>
-            <small>Tuzuvchi: N.Abdullayeva</small>
-            <span>
-              Boshlash<i className="fas fa-arrow-right"></i>
-            </span>
-          </div>
-          <div className="test_card">
-            <small>Test soni: 40ta</small>
-            <h4 className="mb-3">Tabiiy fanlar</h4>
-            <small>Tuzuvchi: N.Abdullayeva</small>
-            <span>
-              Boshlash<i className="fas fa-arrow-right"></i>
-            </span>
-          </div>
-          <div className="test_card" style={{ background: "green" }}>
-            <small>Test soni: 40ta</small>
-            <h4 className="mb-3">Aralash test</h4>
-            <small>Tuzuvchi: N.Abdullayeva</small>
-            <span>
-              Boshlash<i className="fas fa-arrow-right"></i>
-            </span>
-          </div>
+          {categories.map((category) => (
+            <Link
+              to={`/category/${category.slug}`}
+              key={category.id}
+              className="test_card"
+            >
+              <small>Test soni: {categoryTestCount[category.name]}</small>
+              <h4 className="mb-3">{category.name}</h4>
+              <small>Tuzuvchi: N.Abdullayeva</small>
+              <span>
+                Boshlash<i className="fas fa-arrow-right"></i>
+              </span>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
